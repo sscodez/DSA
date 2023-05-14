5e68568ac4de79f47e11ad7d31c31c4b479ad4e4
Syed Sameer Shah <78991984+sscodez@users.noreply.github.com>
2023-05-14 20:16:41 +0500

    Create Rearrange_array_such_that_even_positioned_are_greater_than_odd.java

---
+++ Array/Rearrange_array_such_that_even_positioned_are_greater_than_odd.java
@@ -0,0 +1,48 @@
+// Java program to rearrange the elements
+// in the array such that even positioned are
+// greater than odd positioned elements
+import java.io.*;
+
+class GFG {
+	public static void rearrange(int[] arr, int n)
+	{
+		for (int i = 1; i < n; i++) {
+
+			// if index is even
+			if (i % 2 == 0) {
+				if (arr[i] > arr[i - 1]) {
+
+					// swap two elements
+					int temp = arr[i];
+					arr[i] = arr[i - 1];
+					arr[i - 1] = temp;
+				}
+			}
+
+			// if index is odd
+			else {
+				if (arr[i] < arr[i - 1]) {
+
+					// swap two elements
+					int temp = arr[i];
+					arr[i] = arr[i - 1];
+					arr[i - 1] = temp;
+				}
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+	// Driver code
+	public static void main(String[] args)
+	{
+		int n = 5;
+		int arr[] = { 1, 3, 2, 2, 5 };
+		rearrange(arr, n);
+	}
+}
+
+// This code is contributed by avanitrachhadiya2155
