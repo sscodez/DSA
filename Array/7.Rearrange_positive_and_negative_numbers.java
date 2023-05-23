9d68b58744d20b09f7ad3d49264d4bbce8ad7d02
Syed Sameer Shah <78991984+sscodez@users.noreply.github.com>
2023-05-23 12:10:55 +0500

    Update 7.Rearrange_positive_and_negative_numbers.java

--- Array/7.Rearrange_positive_and_negative_numbers.java
+++ Array/7.Rearrange_positive_and_negative_numbers.java
@@ -49,7 +49,7 @@ class Alternate {
 			System.out.print(arr[i] + " ");
 	}
 
-	/*Driver function to check for above functions*/
+
 	public static void main (String[] args)
 	{
 		int arr[] = {-1, 2, -3, 4, 5, 6, -7, 8, 9};
